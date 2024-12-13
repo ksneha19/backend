f63bb3d46a2a0ad5afdeecf93b9aa7a8e606c428
ksneha19 <sneha.karingula.935@my.csun.edu>
2024-12-13 15:39:56 -0800

    Initial commit for backend projescdt

--- obj/Debug/net8.0/TaskManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76317db904ce0cf95dbd868b20478ca12c2dd37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c7d470ee57c4a0eb4f6a233adf04a8b4e09448")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
